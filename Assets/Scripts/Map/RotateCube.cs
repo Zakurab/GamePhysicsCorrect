0c6aef462f864aad0fe886ec37f0fa7e78a29305
Zakurab <benjie.stevenson@gmail.com>
2016-12-06 10:19:58 -0500

    Version 1.1

---
+++ Assets/Scripts/Map/RotateCube.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCube : MonoBehaviour {
+
+    public float RotationSpeed;
+    public Vector3 RotationAngle = Vector3.up;
+
+    //Monobehavior funcitons
+    private void Update()
+    {
+        this.transform.Rotate(RotationAngle * RotationSpeed * Time.deltaTime);
+    }
+
+
+}
